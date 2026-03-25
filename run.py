dd9ad12d9268e4b0e94b4468f5b92a62c17e5965
Rodrigo Matus <matusrojasr@gmail.com>
2026-03-25 16:34:27 -0300

    feat: landing + sidebar colapsable

--- run.py
+++ run.py
@@ -1,6 +1,18 @@
-from app import create_app, socketio
+import eventlet
+eventlet.monkey_patch()
+
+from app import create_app, socketio, db
 
 app = create_app()
 
+with app.app_context():
+    db.create_all()
+    try:
+        with db.engine.connect() as conn:
+            conn.execute(db.text('ALTER TABLE inscripcion ADD COLUMN solo_en_grupo BOOLEAN DEFAULT 0'))
+            conn.commit()
+    except Exception:
+        pass
+
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app, debug=False)
